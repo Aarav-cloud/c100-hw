46d10264960e6a00babf462241ced86f5a165c52
Aarav-cloud <79704403+Aarav-cloud@users.noreply.github.com>
2021-04-28 20:30:58 +0530

    Add files via upload

---
+++ C100 HW/atm.py
@@ -0,0 +1,16 @@
+class atm(object):
+
+def __init__(self,cardNumber,pinNumber,CashWithdrawal):
+    self.CardNumber= CardNumber
+    self.pinNumber= Pin
+    self.CashWithdrawal = CashWithdrawal
+    self.speedLimit= speedLimit
+
+def start (self):
+    print ("started") 
+
+def stop (self):
+    print ("stopped") 
+
+hdfc = atm("xxxx,xxx,xxxx,0000","xx00","10,000")
+print (atm.start())
